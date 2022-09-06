3e23f134468dac69c4a57a71ac85929cd13f0a0e
Rhytham Patel <rhythamp.404@gmail.com>
2022-09-06 18:28:57 +0530

    implement navigation in order table

--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as Spinner } from './../Assets/svgs/spinner.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchBar from '../Components/SearchBar';
@@ -6,6 +7,7 @@ import { getOrderList, getProductList, getUserList } from '../Redux/actions';
 import { getProductByID, getUserByID } from '../utils/util';
 
 const Orders = () => {
+	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const orderList = useSelector((state) => state.orders.orders);
 	const loading = useSelector((state) => state.orders.loading);
@@ -68,7 +70,12 @@ const Orders = () => {
 									<tbody key={order?.order_id}>
 										<tr className=' bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
 											<td className='py-4 px-10'>{order.order_id}</td>
-											<td className='py-4 px-10 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+											<td
+												className='py-4 px-10 font-medium text-gray-900 whitespace-nowrap dark:text-white cursor-pointer'
+												onClick={() => {
+													navigate(`/products/${order.product_id}`);
+												}}
+											>
 												{
 													getProductByID({
 														id: order.product_id,
@@ -76,7 +83,12 @@ const Orders = () => {
 													})?.name
 												}
 											</td>
-											<td className='py-4 px-10 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+											<td
+												className='py-4 px-10 font-medium text-gray-900 whitespace-nowrap dark:text-white cursor-pointer'
+												onClick={() => {
+													navigate(`/users/${order.user_id}`);
+												}}
+											>
 												{
 													getUserByID({ id: order.user_id, users: userList })
 														?.name
